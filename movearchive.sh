930ed5049cdb089293486cc6ec550ec78fc5d62d
Katboj <arek.insta@gmail.com>
2023-11-28 17:37:20 +0100

    Przeniesiono plik *.jpg do folderu archiwum.

--- movearchive.sh
+++ movearchive.sh
@@ -11,6 +11,9 @@ if [ ! -d "$archiwum_folder" ]; then
     mkdir "$archiwum_folder"
 fi
 
+# Zmienna do śledzenia czy znaleziono pliki do przeniesienia
+found_files=false
+
 # Przenieś pliki .png i .jpg, które nie mają dzisiejszej daty w nazwie, do folderu archiwum
 for file in *.png *.jpg; do
     if [ -f "$file" ]; then
@@ -18,9 +21,16 @@ for file in *.png *.jpg; do
         if ! [[ "$file" == *"$now"* ]]; then
             mv "$file" "$archiwum_folder/"
             echo "Przeniesiono plik $file do folderu archiwum."
+            found_files=true
         fi
     fi
 done
+
+# Sprawdź, czy znaleziono pliki do przeniesienia
+if [ "$found_files" = false ]; then
+    echo "Nie znaleziono plików do przeniesienia. Skrypt zatrzymany."
+    exit 1
+fi
 
 # Przejście do katalogu repozytorium Git
 cd ~/Desktop/obiad
@@ -33,3 +43,4 @@ git commit -m "Przeniesiono plik $file do folderu archiwum."
 
 # Wypchnij zmiany na GitHub
 git push origin main
+
