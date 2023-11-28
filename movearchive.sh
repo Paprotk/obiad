3abec5dab157b533efc5d9268e0f71195d039401
Katboj <arek.insta@gmail.com>
2023-11-28 17:27:41 +0100

    Zmieniono nazwę pliku *.jpg na:

---
+++ movearchive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pobierz aktualną datę w formacie RRMMDDEE
+now=$(date +"%y%m%d")
+
+# Folder archiwum
+archiwum_folder="archiwum"
+
+# Sprawdź, czy folder archiwum istnieje, jeśli nie, utwórz go
+if [ ! -d "$archiwum_folder" ]; then
+    mkdir "$archiwum_folder"
+fi
+
+# Przenieś pliki .png i .jpg, które nie mają dzisiejszej daty w nazwie, do folderu archiwum
+for file in *.png *.jpg; do
+    if [ -f "$file" ]; then
+        # Sprawdź, czy nazwa pliku nie zawiera dzisiejszej daty
+        if ! [[ "$file" == *"$now"* ]]; then
+            mv "$file" "$archiwum_folder/"
+            echo "Przeniesiono plik $file do folderu archiwum."
+        fi
+    fi
+done
+
