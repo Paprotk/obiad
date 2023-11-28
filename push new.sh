7dff954ac881fb5ab944426faea24d3f29ed8a65
Katboj <arek.insta@gmail.com>
2023-11-28 17:14:08 +0100

    rename

--- push new.sh
+++ push new.sh
@@ -3,33 +3,40 @@
 # Pobierz aktualną datę w formacie RRMMDDEE
 now=$(date +"%y%m%d")
 
-# Stwórz nową nazwę pliku
-new_name="${now}.png"
+# Znajdź pliki z rozszerzeniem .png i .jpg, a następnie stwórz nową nazwę pliku
+for file in *.png *.jpg; do
+    if [ -f "$file" ]; then
+        # Pobierz oryginalne rozszerzenie pliku
+        extension="${file##*.}"
+        
+        # Stwórz nową nazwę pliku z oryginalnym rozszerzeniem na końcu
+        new_name="${now}.${extension}"
+        
+        mv "$file" "$new_name"
+        echo "Zmieniono nazwę pliku $file na: $new_name"
+    fi
+done
 
-# Zmień nazwę pliku 1.png na nową nazwę
-mv 1.png "$new_name"
-
-echo "Zmieniono nazwę pliku na: $new_name"
 
 # Sprawdź, czy folder archiwum istnieje, jeśli nie, utwórz go
-archive_folder="archiwum"
-if [ ! -d "$archive_folder" ]; then
-    mkdir "$archive_folder"
-fi
+#archive_folder="archiwum"
+#if [ ! -d "$archive_folder" ]; then
+#    mkdir "$archive_folder"
+#fi
 
 # Przenieś plik do folderu archiwum
-mv "$new_name" "$archive_folder/"
+#mv "$new_name" "$archive_folder/"
 
-echo "Plik przeniesiony do folderu archiwum: $archive_folder/$new_name"
+#echo "Plik przeniesiony do folderu archiwum: $archive_folder/$new_name"
 
 # Przejście do katalogu repozytorium Git
- cd ~/Desktop/obiad
+# cd ~/Desktop/obiad
 
 # Dodaj wszystkie zmiany
-git add .
+#git add .
 
 # Utwórz commit z wiadomością
-git commit -m "Aktualizacja pliku $new_name i przeniesienie do archiwum"
+#git commit -m "Aktualizacja pliku $new_name i przeniesienie do archiwum"
 
 # Wypchnij zmiany na GitHub
-git push origin main
+#git push origin main
